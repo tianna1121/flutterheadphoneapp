c0f398a51ac34fcaf0fc129f8679b405f72d7fe0
Fred Jonason <fredjonason@gmail.com>
2019-08-28 14:45:24 +0800

    Init commit, pretty cool headphone shop app. based on https://www.youtube.com/watch?v=w45GyPYTEU0&t=300s

---
+++ lib/style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final TextStyle searchBarStyle =
+    TextStyle(fontSize: 18, fontFamily: "sofiaPro-light");
+final TextStyle headingStyle =
+    TextStyle(fontSize: 26, fontFamily: "sofiaPro-bold");
+final TextStyle productTitleStyle =
+    TextStyle(fontSize: 18, fontFamily: "sofiaPro-bold");
+final TextStyle productsubTitleStyle =
+    TextStyle(fontFamily: "sofiaPro-regular");
+final TextStyle bottomBarItemStyle =
+    TextStyle(fontSize: 18, color: Colors.white, fontFamily: "sofiaPro-bold");
+final TextStyle speakerdescStyle =
+    TextStyle(fontSize: 16, color: Colors.grey, fontFamily: "sofiaPro-light");
